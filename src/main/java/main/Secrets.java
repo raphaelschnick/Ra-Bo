3baa9f8a0af67fc94c1a7616ede7337cab80bb1e
raphaelschnick <raphael.schnick@sybit.de>
2021-07-20 16:13:21 +0200

    temp prefix

--- src/main/java/main/Secrets.java
+++ src/main/java/main/Secrets.java
@@ -2,9 +2,9 @@ package main;
 
 public class Secrets {
 
-    public static final String TOKEN = "";
+    public static final String TOKEN = "ODY1NTIzMjU5NzAxMTk4ODc5.YPFPWQ.2T4MCUgeZQJFSzOTqSj5iTRU9ho";
 
     public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/ra-bo";
-    public static final String MYSQL_USER = "root";
-    public static final String MYSQL_PASSWORD = "";
+    public static final String MYSQL_USER = "admin";
+    public static final String MYSQL_PASSWORD = "30122003";
 }
